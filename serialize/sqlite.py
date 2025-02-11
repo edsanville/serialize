b71116373a308d666b8949765b354b1b66d2dfdd
Ed Sanville <edsanville@gmail.com>
2025-02-11 09:14:27 -0500

    Added insert function

--- serialize/sqlite.py
+++ serialize/sqlite.py
@@ -3,6 +3,8 @@
 from dataclasses import *
 from typing import *
 import sqlite3
+import os
+
 
 type_map = {
     int: 'integer',
@@ -11,8 +13,18 @@ type_map = {
     bool: 'integer'
 }
 
+
+def normalize(value: any):
+    if type(value) == str:
+        return '"' + value + '"'
+    if type(value) == bool:
+        return str(int(value))
+    else:
+        return str(value)
+
+
 def create_table(db: sqlite3.Connection, table_name: str, Class, key_columns=[]):
-    table_id = f'{table_name}$id'
+    table_id = f'{table_name}_id'
     key_column = table_id + ' integer primary key'
 
     columns = [key_column] + key_columns
@@ -49,17 +61,68 @@ def create_table(db: sqlite3.Connection, table_name: str, Class, key_columns=[])
 
     columns_string = ', '.join(columns)
 
-    db.execute(f'create table {table_name} ({columns_string})')
+    db.execute(f'create table if not exists {table_name} ({columns_string})')
 
 
-def create_tables(db: sqlite3.Connection, Class):
-    for table_name, type_hint in get_type_hints(Class).items():
-        assert(get_origin(type_hint) == list)
-        create_table(db, table_name, get_args(type_hint)[0])
+def insert(db: sqlite3.Connection, table_name: str, obj, other_columns={}):
+    # obj is python class, List[Class], or Dict[str, Class]
+    cursor = db.cursor()
+    Class = type(obj)
+
+    columns: dict = other_columns
+
+    if Class in type_map:
+        columns['value'] = obj
+
+        column_names = ', '.join(columns.keys())
+        value_placeholders = ', '.join(['?'] * len(columns))
+        cursor.execute(f'insert into {table_name} ({column_names}) values ({value_placeholders})', list(columns.values()))
+    else:
+        type_hints = get_type_hints(Class)
+
+        for var_name, type_hint in type_hints.items():
+            value = getattr(obj, var_name)
+
+            if type_hint in type_map:
+                columns[var_name] = value
+                continue
+        
+        print(obj)
+
+        if len(columns) > 0:
+            column_names = ', '.join(columns.keys())
+            value_placeholders = ', '.join(['?'] * len(columns))
+            cursor.execute(f'insert into {table_name} ({column_names}) values ({value_placeholders})', list(columns.values()))
+        else:
+            cursor.execute(f'insert into {table_name} default values')
+
+        id = cursor.lastrowid
+
+        for var_name, type_hint in type_hints.items():
+            value = getattr(obj, var_name)
+            origin = get_origin(type_hint)
+            args = get_args(type_hint)
+
+            if origin == list:
+                assert(len(args) == 1)
+                for item in value:
+                    insert(db, table_name + '$' + var_name, item, other_columns={table_name + '_id': id})
+
+            if origin == dict:
+                assert(len(args) == 2)
+                for key, item in value.items():
+                    insert(db, table_name + '$' + var_name, item, other_columns={table_name + '_id': id, 'key': key})
+    
+
 
 
 def main():
-    db = sqlite3.connect('test.sqlite')
+    db_file = 'test.sqlite'
+
+    if os.path.exists(db_file):
+        os.remove(db_file)
+
+    db = sqlite3.connect(db_file)
 
     @dataclass
     class AnotherClass:
@@ -69,10 +132,14 @@ def main():
 
     @dataclass
     class Class:
-        c: List[AnotherClass]
+        a: List[int]
 
-    create_tables(db, Class)
+    obj = Class(a=[1, 2, 3, 4])
 
+    create_table(db, 'objects', Class)
+    insert(db, 'objects', obj)
+
+    db.commit()
 
 if __name__ == '__main__':
     main()
